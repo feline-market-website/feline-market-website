43634544ea26f5edb8970ab1f3c8c88ba4166fde
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-11-21 11:34:03 +0700

    Fix app default port

--- feline-market-nestjs/src/main.ts
+++ feline-market-nestjs/src/main.ts
@@ -3,6 +3,6 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(String(process.env.PORT));
 }
 bootstrap();
