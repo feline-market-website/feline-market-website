323def01d80dcdecfcb2dbb8e4c039f812e75867
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-11-21 11:33:48 +0700

    Use DataSource abstraction to implement Checking Database Connection

--- feline-market-nestjs/src/app.module.ts
+++ feline-market-nestjs/src/app.module.ts
@@ -1,8 +1,9 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config";
 import { DataSource } from 'typeorm';
-import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
@@ -29,6 +30,19 @@ import { UsersModule } from './users/users.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
+export class AppModule implements OnModuleInit {
   constructor(private dataSource:DataSource) {}
+
+  async onModuleInit() {
+    try {
+      if (!this.dataSource.isInitialized) {
+        await this.dataSource.initialize(); // Checking Database connection status.
+        console.log('Database connection established successfully.');
+      } else {
+        console.log('Database connection is already initialized.');
+      }
+    } catch (error) {
+      console.error('Database connection failed:', error);
+    }
+  }
 }
