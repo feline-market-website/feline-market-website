d377c80a4b3b44fb6bfdecaad2677f4986cebf31
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-11-22 23:20:04 +0700

    Update role class name

--- feline-market-nestjs/src/roles/roles.module.ts
+++ feline-market-nestjs/src/roles/roles.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { Roles } from './entities/roles.entity';
+import { Role } from './entities/roles.entity';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Roles])],
+  imports: [TypeOrmModule.forFeature([Role])],
   controllers: [RolesController],
   providers: [RolesService],
 })
--- feline-market-nestjs/src/roles/roles.service.ts
+++ feline-market-nestjs/src/roles/roles.service.ts
@@ -1,32 +1,32 @@
 import { CreateRoleDto } from './dto/create-roles.dto';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { Roles } from './entities/roles.entity';
+import { Role } from './entities/roles.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { validate } from 'uuid';
 
 @Injectable()
 export class RolesService {
   constructor(
-    @InjectRepository(Roles) private roleRepository: Repository<Roles>,
+    @InjectRepository(Role) private roleRepository: Repository<Role>,
   ) {}
 
-  async create(createRoleDto: CreateRoleDto): Promise<Roles> {
+  async create(createRoleDto: CreateRoleDto): Promise<Role> {
     return await this.roleRepository.save(createRoleDto);
   }
 
-  async findAll(): Promise<Roles[]> {
+  async findAll(): Promise<Role[]> {
     return await this.roleRepository.find();
   }
 
-  async findOne(id: string): Promise<Roles> {
+  async findOne(id: string): Promise<Role> {
     if (!validate(id)) {
       throw new BadRequestException(`Invalid UUID format: ${id}`);
     }
     return await this.roleRepository.findOneByOrFail({ id: id });
   }
 
-  async remove(id: string): Promise<Roles> {
+  async remove(id: string): Promise<Role> {
     if (!validate(id)) {
       throw new BadRequestException(`Invalid UUID format: ${id}`);
     }
