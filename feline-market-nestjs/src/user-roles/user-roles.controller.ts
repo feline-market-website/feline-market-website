1ad30576029c444c11707a28450300fcb3eaebf2
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-12-21 13:15:16 +0700

    Refactoring and error handling to user-roles api services

--- feline-market-nestjs/src/user-roles/user-roles.controller.ts
+++ feline-market-nestjs/src/user-roles/user-roles.controller.ts
@@ -1,26 +1,46 @@
-import { Body, Controller, Get, Param,Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 
 import { UserRolesService } from './user-roles.service';
 import { AssignRoleDto } from './dto/assign-role-dto';
 import { UserRole } from './entities/user-role.entity';
 import { RemoveRoleDto } from './dto/remove-role-dto';
+import { RolesService } from 'src/roles/roles.service';
 
 @Controller('user-roles')
 export class UserRolesController {
-  constructor(private readonly userRolesService: UserRolesService) {}
+  constructor(
+    private readonly userRolesService: UserRolesService,
+    private readonly rolesService: RolesService,
+  ) {}
 
   @Post()
-  async assignRole(@Body() assignRoleDto: AssignRoleDto): Promise<UserRole> {
-    return this.userRolesService.assignRoleToUser(assignRoleDto);
+  @HttpCode(HttpStatus.OK)
+  async assignRole(
+    @Body() assignRoleDto: AssignRoleDto,
+  ): Promise<{ message: string; data: UserRole }> {
+    const response =
+      await this.userRolesService.assignRoleToUser(assignRoleDto);
+    const role = await this.rolesService.findOne(assignRoleDto.role_id);
+    return {
+      message: `Assigned role ${role.name} to the user successfully`,
+      data: response,
+    };
   }
 
-  @Get(':userId')
-  async getUserRole(@Param('userId') userId: string): Promise<UserRole[]> {
-    return this.userRolesService.getRoleForUser(userId);
-  }
-
-  @Post()
-  async removeRole(@Body() removeRoleDto: RemoveRoleDto): Promise<UserRole> {
-    return this.userRolesService.removeRoleFromUser(removeRoleDto);
+  @Delete()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async removeRole(
+    @Body() removeRoleDto: RemoveRoleDto,
+  ): Promise<{ message: string }> {
+    await this.userRolesService.removeRoleFromUser(removeRoleDto);
+    const role = await this.rolesService.findOne(removeRoleDto.role_id);
+    return { message: `Removed role ${role.name} for the user successfully` };
   }
 }
--- feline-market-nestjs/src/user-roles/user-roles.service.ts
+++ feline-market-nestjs/src/user-roles/user-roles.service.ts
@@ -1,11 +1,15 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRole } from './entities/user-role.entity';
 import { Repository } from 'typeorm';
 import { AssignRoleDto } from './dto/assign-role-dto';
-import {validate} from 'uuid'
+import { validate } from 'uuid';
 import { RemoveRoleDto } from './dto/remove-role-dto';
-
 
 @Injectable()
 export class UserRolesService {
@@ -15,22 +19,46 @@ export class UserRolesService {
   ) {}
 
   async assignRoleToUser(dto: AssignRoleDto): Promise<UserRole> {
-    const userRole = this.userRoleRepository.create({user: {id: dto.user_id}, role: {id: dto.role_id}})
-    return this.userRoleRepository.save(userRole);
+    try {
+      const userRole = this.userRoleRepository.create({
+        user: { id: dto.user_id },
+        role: { id: dto.role_id },
+      });
+      return this.userRoleRepository.save(userRole);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `An error occurred while assigning role to the user: ${error.message}`,
+      );
+    }
   }
 
   async getRoleForUser(userId: string): Promise<UserRole[]> {
-    if (!validate(userId)) {
-      throw new BadRequestException(`Invalid UUID format: ${userId}`);
+    try {
+      if (!validate(userId)) {
+        throw new BadRequestException(`Invalid UUID format: ${userId}`);
+      }
+      return this.userRoleRepository.find({
+        where: { user: { id: userId } },
+        relations: ['roles'],
+      });
+    } catch (error) {
+      throw new NotFoundException(
+        `UserRole for userId Not Found: ${error.message}`,
+      );
     }
-    return this.userRoleRepository.find({
-      where: {user: {id: userId}},
-      relations: ['roles'],
-    });
   }
 
   async removeRoleFromUser(dto: RemoveRoleDto): Promise<UserRole> {
-    const userRole = this.userRoleRepository.create({user: {id: dto.user_id}, role: {id: dto.role_id}})
-    return this.userRoleRepository.remove(userRole);
+    try {
+      const userRole = await this.userRoleRepository.findOneOrFail({
+        where: { user: { id: dto.user_id }, role: { id: dto.role_id } },
+        relations: ['role', 'user']
+      });
+      return this.userRoleRepository.remove(userRole);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `An error occurred while removing role to the user: ${error.message}`,
+      );
+    }
   }
 }
