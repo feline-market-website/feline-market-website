d64310dad1141c02c17c8100a63374522b5d8ed8
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-12-26 01:56:34 +0700

    Loading animation while login

--- feline-market-nextjs/src/components/login/LoginForm.tsx
+++ feline-market-nextjs/src/components/login/LoginForm.tsx
@@ -8,10 +8,11 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
+import React, { useState } from "react";
 
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-import React from "react";
+import { Loader2 } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { useToast } from "@/hooks/use-toast";
 import { z } from "zod";
@@ -29,6 +30,7 @@ interface ComponentProps {
 }
 
 const LoginForm: React.FC<ComponentProps> = ({ onSubmit }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const { toast } = useToast();
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
@@ -39,18 +41,29 @@ const LoginForm: React.FC<ComponentProps> = ({ onSubmit }) => {
   });
 
   const secondarySubmit = async (values: z.infer<typeof loginSchema>) => {
-    const data = await onSubmit(values);
-    if (!data.access_token) {
+    setIsLoading(true);
+    try {
+      const data = await onSubmit(values);
+      if (!data.access_token) {
+        toast({
+          variant: "destructive",
+          title: "Login Fail",
+          description: `Wrong Username or Password`,
+        });
+      } else {
+        toast({
+          title: "Login Successfully ✅",
+          description: `You login with username: ${values.username}`,
+        });
+      }
+    } catch {
       toast({
         variant: "destructive",
-        title: "Login Fail",
-        description: `Wrong Username or Password`,
+        title: "Error",
+        description: `An error occurred while login in`,
       });
-    } else {
-      toast({
-        title: "Login Successfully ✅",
-        description: `You login with username: ${values.username}`,
-      });
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -59,35 +72,42 @@ const LoginForm: React.FC<ComponentProps> = ({ onSubmit }) => {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(secondarySubmit)}>
           <div className="flex flex-col gap-4">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="Username" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input type="password" placeholder="Password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
           <div className="my-8 flex flex-col">
-          <Button type="submit">Login</Button>
+            {isLoading ? (
+              <Button disabled>
+                <Loader2 className="animate-spin" />
+                Please wait
+              </Button>
+            ) : (
+              <Button type="submit">Login</Button>
+            )}
           </div>
         </form>
       </Form>
