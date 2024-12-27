e4ab3bc30d4e32b12b4b05d2c6ad9e6ea8613c07
Wissarut Kanasub <wissarut.dev@gmail.com>
2024-12-27 17:33:32 +0700

    Impletment action getMe

---
+++ feline-market-nextjs/src/actions/auth/getMeAction.ts
@@ -0,0 +1,24 @@
+"use server"
+
+import { User } from "@/utils/type";
+import { cookies } from "next/headers"
+
+export default async function getMe(): Promise<User | null> {
+    try {
+        const cookieString = (await cookies()).toString();
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/auth/me`, {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                Cookie: cookieString,
+            }
+        });
+        if (!response.ok) {
+            throw new Error
+        }
+        const data = await response.json();
+        return data;
+    } catch {
+        return null
+    }
+}
